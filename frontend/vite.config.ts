1e6eca9f539e545adaf7beb2d28d2e1667c6e55c
Chris Arsenault <carsena2@gmail.com>
2026-04-19 04:04:20 -0700

    Fix Vite worker build output

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  worker: {
+    format: "es",
+  },
   server: {
     proxy: {
       "/api": "http://localhost:8080",
